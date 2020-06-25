6470a770ddc1f57d69b0533f2a21991d39c9c622
Anoushka Jain <anoushkajain@beate-wexeler.wexeler.local>
2020-06-25 17:38:00 +0200

    Updates

---
+++ precache-manifest.fc12b8c841bc3d8bf9fa701185478aa6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8cabc2d7d8df887417317b8b38e242e",
+    "url": "/fitapp/index.html"
+  },
+  {
+    "revision": "45d3db4f9f7b4b384dda",
+    "url": "/fitapp/static/css/main.529f9184.chunk.css"
+  },
+  {
+    "revision": "4281131dfee340ff93e2",
+    "url": "/fitapp/static/js/2.9d47d495.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/fitapp/static/js/2.9d47d495.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45d3db4f9f7b4b384dda",
+    "url": "/fitapp/static/js/main.25f79835.chunk.js"
+  },
+  {
+    "revision": "8df8f455a021ff1b84e5",
+    "url": "/fitapp/static/js/runtime-main.996c5af8.js"
+  }
+]);
